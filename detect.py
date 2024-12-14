90cb3a0503deaa3c6b239c233a9cd0f4c2022e6b
Trần Thị Diễm Quỳnh <149855210+daylaquynh2003@users.noreply.github.com>
2024-12-14 16:11:39 +0700

    Add files via upload

---
+++ detect.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+import cvzone
+import cv2
+import math
+# Running real time from webcam
+cap = cv2.VideoCapture(0)
+model = YOLO('fire.pt')
+
+
+# Reading the classes
+classnames = ['fire']
+
+
+while True:
+    ret,frame = cap.read()
+    result = model(frame,stream=True)
+
+    # Getting bbox,confidence and class names informations to work with
+    for info in result:
+        boxes = info.boxes
+        for box in boxes:
+            confidence = box.conf[0]
+            confidence = math.ceil(confidence * 100)
+            Class = int(box.cls[0])
+            if confidence > 55:
+                x1,y1,x2,y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
+                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,255),2)
+                cvzone.putTextRect(frame, f'{classnames[Class]} {confidence}%', [x1 + 8, y1 - 12],
+                                   scale=1.5,thickness=2)
+
+
+
+
+    cv2.imshow('frame',frame)
+    cv2.waitKey(1)
